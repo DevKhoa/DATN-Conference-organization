b10e3030467e311aa40338542708c63d523450ea
minhman21032004 <phamminhman19052018@gmail.com>
2026-05-28 12:49:41 +0700

    Delete conferences_.$conferenceId.papers.$paperId.qa.tsx

--- client/src/routes/conferences_.$conferenceId.papers.$paperId.qa.tsx
+++
@@ -1,8 +0,0 @@
-import PaperQAPage from "@/pages/main/public/PaperQA";
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute(
-  "/conferences_/$conferenceId/papers/$paperId/qa",
-)({
-  component: PaperQAPage,
-});
